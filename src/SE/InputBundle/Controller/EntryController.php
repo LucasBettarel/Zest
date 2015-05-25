863f54c2d9c95fd124f62cbd22614709ec0ec92f
LucasBettarel <lucas.bettarel@gmail.com>
2015-05-25 17:43:18 +0800

    input form step1

--- src/SE/InputBundle/Controller/EntryController.php
+++ src/SE/InputBundle/Controller/EntryController.php
@@ -2,13 +2,31 @@
 
 namespace SE\InputBundle\Controller;
 
+use SE\InputBundle\Entity\UserInput;
+use SE\InputBundle\Form\UserInputType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class EntryController extends Controller
 {
-	public function indexAction()
+	public function inputAction(Request $request)
 	{
-		return $this->render('SEInputBundle:Entry:index.html.twig');
+	  $userInput = new UserInput();
+      $form = $this->createForm(new UserInputType(), $userInput);
+
+      if ($form->handleRequest($request)->isValid()) {
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($userInput);
+        $em->flush();
+
+      $request->getSession()->getFlashBag()->add('notice', 'new working hours entry saved');
+
+      return $this->redirect($this->generateUrl('se_input_home'));
+      }
+
+      return $this->render('SEInputBundle:Entry:input.html.twig', array(
+      'form' => $form->createView(),
+      ));
 	}
 
 	public function menuAction()
---
+++ src/SE/InputBundle/Form/UserInputType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace SE\InputBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserInputType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('input_entries')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'SE\InputBundle\Entity\UserInput'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'se_inputbundle_userinput';
+    }
+}
