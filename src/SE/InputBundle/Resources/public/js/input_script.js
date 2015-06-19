20f3347fad35fe5c480d2fa8e7bd972570641adc
LucasBettarel <lucas.bettarel@gmail.com>
2015-06-19 16:30:26 +0800

    disable function

--- src/SE/InputBundle/Resources/public/js/input_script.js
+++ src/SE/InputBundle/Resources/public/js/input_script.js
@@ -10,4 +10,41 @@ $(document).ready(function() {
 	$(document).on('click', '[data-toggle="popover"]', function(e) {
 		$(this).popover();
 	});
+
+	$(document).on('click', '.transfer', function(e){
+		$activitiesHolder = $("#"+$(this).attr('data-sub-target'));
+		$rowHolder = $('*[data-content="'+$(this).attr('data-target')+'"]');
+		//****************************************************************************
+		//manque le cas ou on disable qu'une activite, puis qu'on supprime les autres
+
+		if ($activitiesHolder.attr('data-counter') == 1){
+			//disable toute la ligne, sauf transfer
+			if($(this).attr('data-disabled') == 'false'){
+				//disable que l'activite, sauf transfer et ajoute la propriete a transfer
+				$(rowHolder).find("input, select, textarea").not('.transfer').prop('disabled', true);
+				$(this).attr('data-disabled', 'true');
+				console.log('1 ligne disable');
+			}
+			else{
+				$rowHolder.find("input, select, textarea").not('.transfer').prop('disabled', false);
+				$(this).attr('data-disabled', 'false');
+				console.log('1 ligne enable');
+			}
+		}
+		else{
+			if($(this).attr('data-disabled') == 'false'){
+				//disable que l'activite, sauf transfer et ajoute la propriete a transfer
+				$(this).closest('row').find("input, select").not('.transfer').prop('disabled', true);
+				$(this).attr('data-disabled', 'true');
+				console.log('x ligne disable');
+			}
+			else{
+				$(this).closest('row').find("input, select").not('.transfer').prop('disabled', false);
+				$(this).attr('data-disabled', 'false');
+				console.log('x ligne enable');
+			}
+		}
+	});
+
+	$('form').children('div').last().hide();
 });
