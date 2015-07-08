6bf93adef6e24c9ae31a207203ed4a931dc88060
LucasBettarel <lucas.bettarel@gmail.com>
2015-07-08 17:52:41 +0800

    review page : datattable + highchrt

--- src/SE/InputBundle/Resources/public/js/review.js
+++ src/SE/InputBundle/Resources/public/js/review.js
@@ -1,16 +1,65 @@
 $(document).ready(function() {
+	totalError = importErrors+inputErrors+toErrors+hourErrors;
 
-	  $('#table').DataTable( {
-	  	paging: false,
+	$('#container').highcharts({
+        chart: {
+            type: 'pie'
+        },
+        title: {
+            text: totalError+" errors to review!"
+        },
+        legend: {
+            align: 'right',
+            verticalAlign: 'top',
+            layout: 'vertical',
+            x: 0,
+            y: 100
+        },
+        yAxis: {
+            title: {
+                text: 'error percentage'
+            }
+        },
+        plotOptions: {
+            pie: {
+                shadow: false
+            }
+        },
+        tooltip: {
+            formatter: function() {
+                return '<b>'+ this.point.name +'</b>: '+ this.y +' %';
+            }
+        },
+        series: [{
+            name: 'error',
+            data: [["Missing SAP imports",importErrors],["Missing inputs",inputErrors],["TO line not affected",toErrors], ["Missing manhours", hourErrors]],
+            size: '100%',
+            innerSize: '60%',
+            showInLegend:true,
+            dataLabels: {
+                enabled: false
+            }
+        }]
+    });
+
+	$('#table').DataTable( {
+		paging: false,
 	  	"dom": 'lrtip',
 	  	"info": false
-	  });
+	});
 
-	  $('#filters a').click(function(){
+	$('#errors').DataTable( {
+		"scrollY":        "150px",
+		paging: false,
+	  	"dom": 'lrtip',
+	  	"info": false
+	});
+
+	$('#filters a').click(function(){
 	  	filterColumn( $(this).parents('div').attr('id'), $(this).attr('id') );
 	  	$(this).siblings().removeClass('label-primary').addClass('label-default');
 	  	$(this).removeClass('label-default').addClass('label-primary');
-	  })
+	});
 
 });
 
