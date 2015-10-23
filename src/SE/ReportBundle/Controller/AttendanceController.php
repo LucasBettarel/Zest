2068329b65d43e1bf90a97059d4904d4aaebf043
LucasBettarel <lucas.bettarel@gmail.com>
2015-10-23 16:31:16 +0800

    attendance step 2: backend almost done

--- src/SE/ReportBundle/Controller/AttendanceController.php
+++ src/SE/ReportBundle/Controller/AttendanceController.php
@@ -22,7 +22,7 @@ class AttendanceController extends Controller
    		$month = date('n');
    		$calendar = $em->getRepository('SEReportBundle:Calendar')->getMonth($month, $year);
 	   
-	    return $this->render('SEReportBundle:Productivity:hr.html.twig', array(
+	    return $this->render('SEReportBundle:Attendance:hr.html.twig', array(
     		'calendar' => $calendar,
     		'employees' => $employees
     	)); 
@@ -104,5 +104,109 @@ class AttendanceController extends Controller
 
 	public function loaddAttData($ts, $i, $d){
 		return $d;
+	}
+
+	public function employeesAttendanceAction()
+	{ 
+	    $em = $this->getDoctrine()->getManager();
+	    $request = $this->get('request');        
+	    $year = $request->get('year');
+	    $month = $request->get('month');
+	    $daysNb = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+	    $hourStructure = array('presence' => 0, 'absence' => 0, 'tothr' => 0, 'reghr' => 0, 'othr' => 0);
+	    //$infoStructure = array('id' => 0, 'name' => 0, 'sesa' => 0, 'dTeam' => 0, 'dShift' => 0, 'dActivity' => 0);	
+		$userInputs = $em->getRepository('SEInputBundle:UserInput')->getMonthInputs($month,$year);
+   		$employees = $em->getRepository('SEInputBundle:Employee')->getAlphaEmployees();
+   		$jsonAttendance = array();
+
+		//create employee-array structure
+		foreach ($employees as $employee) {
+			//create html name cell
+			$eCell = "<div id='".$employee->getId()."' class='e-name' data-toggle='tooltip' data-placement='right' title='<strong>".$employee->getDefaultTeam()->getName()." - Shift ".$employee->getDefaultShift()->getId()."</strong><br>".$employee->getSesa()."<br>".$employee->getDefaultActivity()->getName()."'>".$employee->getName()."</div>";
+			$jsonAttendance[$employee->getId()] = array('employee' => $eCell, 'team' => $employee->getDefaultTeam()->getName(), 'shift' => $employee->getDefaultShift()->getId(), 'total'=>0);
+			for ($i=0; $i < $daysNb; $i++) { 
+				$jsonAttendance[$employee->getId()][($i+1)] = $hourStructure;
+			}
+		}
+
+		//fill data
+		foreach ($userInputs as $userInput) {
+			$dateInput = $userInput->getDateInput()->format("j");
+			//when details of activities in tooltip -> team+shift required here
+			foreach ($userInput->getInputEntries() as $inputEntry) {
+				$regtohr = 0;
+				$ottohr = 0;
+				$employeeId = $inputEntry->getEmployee()->getId();
+				if($inputEntry->getPresent()){
+					//go into activities to remove this fucking transfer out mess...
+					//TODO : update when this transfer out fuck will be better
+					if($inputEntry->getActivityHours()){
+						foreach ($inputEntry->getActivityHours() as $activityHour) {
+						 	if($activityHour->getActivity()->getId() == 13){
+						 		$regtohr = $activityHour->getRegularHours();
+						 		$ottohr = $activityHour->getOtHours();
+						 		break;
+						 	}
+						}
+					}
+					//update hours
+					$jsonAttendance[$employeeId][$dateInput]['presence'] = 1;
+					$jsonAttendance[$employeeId][$dateInput]['absence'] = 0;
+					$jsonAttendance[$employeeId][$dateInput]['othr'] += $inputEntry->getTotalOvertime() - $ottohr;
+					$jsonAttendance[$employeeId][$dateInput]['reghr'] += $inputEntry->getTotalHours() - $inputEntry->getTotalOvertime() - $regtohr;
+					$jsonAttendance[$employeeId][$dateInput]['tothr'] += $inputEntry->getTotalHours() - $ottohr - $regtohr;
+				}else{
+					$jsonAttendance[$employeeId][$dateInput]['presence'] = 0;
+					//TODO : Update when halfdays will be managed
+					$jsonAttendance[$employeeId][$dateInput]['absence'] = $inputEntry->getAbsenceReason()->getName();
+				}
+			}
+		}
+
+		//calcul monthly total and formate to html
+		for ($j=1; $j < sizeof($jsonAttendance); $j++) { 
+		 	if(isset($jsonAttendance[$j])){
+			 		for ($i=1; $i <= $daysNb; $i++) { 
+					$jsonAttendance[$j]['total'] += $jsonAttendance[$j][$i]['tothr'];
+					//test to identify errors in hours reported
+					if($jsonAttendance[$j][$i]['presence'] == 0){
+						if($jsonAttendance[$j][$i]['absence'] != 0){//leave
+							$dCell = "<div id='".$i."' class='e-day-leave'><i class='glyphicon glyphicon-error'> </i> ".$jsonAttendance[$j][$i]['absence']."</div>";
+						}else{//not working
+							$dCell = "<div id='".$i."' class='e-day-absent'></div>";	
+						}
+					}elseif($jsonAttendance[$j][$i]['tothr'] > 11 || $jsonAttendance[$j][$i]['othr'] > 8 || $jsonAttendance[$j][$i]['reghr'] > 8){//warning too much hour
+						$dCell = "<div id='".$i."' class='e-day-high'><i class='glyphicon glyphicon-exclamation-sign'> </i> ".$jsonAttendance[$j][$i]['tothr']."</div>";
+					}elseif( $jsonAttendance[$j][$i]['reghr'] < 7.5 ){//missing hour
+						$dCell = "<div id='".$i."' class='e-day-low'><i class='glyphicon glyphicon-exclamation-sign'> </i> ".$jsonAttendance[$j][$i]['tothr']."</div>";					
+					}else{//ok
+						$dCell = "<div id='".$i."' class='e-day-ok'><i class='glyphicon glyphicon-ok'> </i> ".$jsonAttendance[$j][$i]['tothr']."</div>";					
+					}
+					$jsonAttendance[$j][$i] = $dCell; 
+				}
+			}
+		}
+		
+		$response = array("code" => 100, "success" => true, "jsonAttendance" => $jsonAttendance);
+	    
+	    return new Response(json_encode($response)); 
+	}
+
+	public function createJson($structure)
+	{
+		$em = $this->getDoctrine()->getManager();
+		$teams = $em->getRepository('SEInputBundle:Team')->findAll();
+		$json = array();
+		$json[0][0] = $structure;
+
+		foreach ($teams as $team) {
+			$teamId = $team->getId();
+			$json[$teamId][0] = $structure;
+			for ($i=0; $i < $team->getShiftnb(); $i++) { 
+				$shiftId = $i+1;
+				$json[$teamId][$shiftId] = $structure;
+			}
+		}
+		return $json;
 	}
 }
