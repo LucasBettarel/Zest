a289d183d1d83bcb0522fdb9a878c94e1eab3960
LucasBettarel <lucas.bettarel@gmail.com>
2016-01-06 17:34:34 +0800

    halfday front end input ok

--- src/SE/InputBundle/Resources/public/js/input/collection.js
+++ src/SE/InputBundle/Resources/public/js/input/collection.js
@@ -63,171 +63,155 @@ $(document).ready(function() {
       return false;
     });
 
-    $(document).on('click', '#presence :checkbox', function(e) {
-      $(this).closest('#presence').find('.toggling').toggleClass('hide');
-      $(this).closest('td').toggleClass('expand-cell');
-      var $actProto = $(this).closest('td').siblings('#activities').find('div:first');
-      if ($(this).closest('#presence').find('.toggling').hasClass('hide')){
-        //add 1st activity
-        addSubElement($actProto);
-        $(this).closest('#presence').find('.input-reason').val(0);
-      }
-      else{
-        //rmv activities
-        $actProto.children().remove();
-        $(this).closest('#presence').find('.input-reason').val(1);
-      }
+    $(document).on('click', '#presence-container, presence-container *', function(e) {
+      presenceToggler($(this).closest("#presence-container"));
     });
 
   $(document).on('click', '#comment', function(e) {
     $(this).parent().siblings('.txtarea-sm').toggleClass('hide');
   });
 
-  $(document).on('click', '#manual', function(e) {
-    $(this).siblings('.input-manual').toggleClass('hide');
-  });
-
-    function addElement($prototypeHolder, values){
-      if($prototypeHolder.is("#entries-prototype")){
-        var $element = definePrototype($prototypeHolder, true);
-        $('tbody#entries').append($element);
-        var $item = $('tbody#entries').children().last();
-
-        //attach data-target on rmv btn and remove btn on item appended
-        var content = $prototypeHolder.attr('id')+'_'+$prototypeHolder.attr('data-counter');
-        attachData($item, content);
-
-        //get activity prototype
-        var $activitiesPrototypeHolder = $item.find('#activities-prototype');
-        $activitiesPrototypeHolder.attr('id',content+'_activities-prototype');
-
-        //add first activity
-        var $sub = addSubElement($activitiesPrototypeHolder);
-        $item.find('#add').attr('data-target', $activitiesPrototypeHolder.attr('id'));
-
-        //increment
-        $prototypeHolder.attr('data-counter', Number($prototypeHolder.attr('data-counter')) + 1);
-
-        $item.find('.input-employee select').val(values[0]);
-        $item.find('.input-sesa input').val(values[2]);
-        $item.find('.input-reason').val(0);
-        $item.find("*[data-toggle='tooltip']").tooltip();
-      }
-      else{
-        var $sub = addSubElement($prototypeHolder);
-        $sub.find("*[data-toggle='tooltip']").tooltip();
-      }
-       //set default values
-       $sub.find('.input-activity select').val(values[5]);
-    }
-
-    function removeElement($element){
-      target = $element.attr('data-target');
-      $('*[data-content="'+target+'"]').remove();
-    }
-
-    function definePrototype($collectionHolder, level){
-      if (!$collectionHolder.attr('data-counter')) {
-        $collectionHolder.attr('data-counter', $collectionHolder.children().length);
-      }
-
-      var $prototype = $collectionHolder.attr('data-prototype');
-      var type = (level) ? "entry_name" : "activity_name";
-      var re = new RegExp(type,"g")
-      var $form = $prototype.replace(re, $collectionHolder.attr('data-counter')).replace(/time_name/g, parseInt($collectionHolder.attr('data-counter'))+1);
-
-      return $form;
-
-    }
-
-    function addSubElement($prototypeHolder){
-      var $element = definePrototype($prototypeHolder, false);
-      $prototypeHolder.append($element);
-      var $sub = $prototypeHolder.children().last();
-      var content = $prototypeHolder.attr('id')+'_'+$prototypeHolder.attr('data-counter');
-      attachData($sub, content);
-      var parentContent = $prototypeHolder.closest('tr').attr('data-content');
-      var $transfer = $sub.find('.transfer').attr({
-        'data-sub-target': $prototypeHolder.attr('id'),
-        'data-target': parentContent,
-        'data-disabled': 0});
-      $sub.find('.input-regular-hours input').val(8);
-      $sub.find('.input-overtime input').val(0);
-      //$sub.find('.input-zone select').val(0);
-      $prototypeHolder.attr('data-counter', Number($prototypeHolder.attr('data-counter')) + 1);
-
-      return $sub;
-    }
-
-    function attachData($item, content){
-      if (!$item.attr('data-content')) {
-          $item.attr('data-content', content);
-        }
-      $item.find('#rmv:last').attr('data-target', content);
-    }
-
-    function dynamicList(valueSelect){
-    //delete all current field
-    $('#entries').children().remove();
-
-    //reset data counter
-    $('#entries-prototype').attr('data-counter', 0);
-
-    //set new overtime default
-    switch(valueSelect){
-      case '2':
-        $('.input-ot-time input:first').val("13:00");
-        $('.input-ot-time input:last').val("01:00");
-        break;
-      case '3':
-        $('.input-ot-time input:first').val("19:30");
-        $('.input-ot-time input:last').val("07:30");
-        break;
-      default:
-        $('.input-ot-time input:first').val("07:30");
-        $('.input-ot-time input:last').val("19:30");
-    }
-
-    //add good nb fields
-    for(var i = 0; i < employeesData.length; ++i){
-          if(employeesData[i][3] == $('.input-team').val() && employeesData[i][4] == valueSelect){
-            addElement($('#entries-prototype'), employeesData[i]);
-          }
-      }
-    }
-
-    function initCollection(){
-      if($.trim( $('#errors').html() ).length ) {
-        $('#errors').parent().toggleClass('hide');
-        alert('There are errors in this input, please check.');
-        }
-      else{
-        $('.input-ot-time input:first').val('07:30');
-        $('.input-ot-time input:last').val('19:00');
-        $('.input-shift').val(1);
-        $(".input-shift input[value=1]").attr('checked', 'checked');
-        for(var i = 0; i < employeesData.length; ++i){
-          if(employeesData[i][3] == $('.input-team').val() && employeesData[i][4] == $('.input-shift').val()){
-            addElement($('#entries-prototype'), employeesData[i]);
-          }
-        } 
-        $('form').children('div').last().hide();
-      }
-      $('.clockpicker').clockpicker(); 
-      $("*[data-toggle='tooltip']").tooltip();
-    }
-
-    function displayOvertime(start, end, $overt){
-      end = end.split(/:/);
-      start = start.split(/:/);
-      var diff = Math.round((end[0] * 3600 + end[1] * 60 + 43200 - (start[0] * 3600 + start[1] * 60 + 43200))/36)/100;
-      if (diff<0){
-        diff+=24;
-      }
-      $overt.val(diff);
-    }
-
 });
+
+function addElement($prototypeHolder, values){
+  if($prototypeHolder.is("#entries-prototype")){
+    var $element = definePrototype($prototypeHolder, true);
+    $('tbody#entries').append($element);
+    var $item = $('tbody#entries').children().last();
+
+    //attach data-target on rmv btn and remove btn on item appended
+    var content = $prototypeHolder.attr('id')+'_'+$prototypeHolder.attr('data-counter');
+    attachData($item, content);
+
+    //get activity prototype
+    var $activitiesPrototypeHolder = $item.find('#activities-prototype');
+    $activitiesPrototypeHolder.attr('id',content+'_activities-prototype');
+
+    //add first activity
+    var $sub = addSubElement($activitiesPrototypeHolder);
+    $item.find('#add').attr('data-target', $activitiesPrototypeHolder.attr('id'));
+
+    //increment
+    $prototypeHolder.attr('data-counter', Number($prototypeHolder.attr('data-counter')) + 1);
+
+    $item.find('.input-employee select').val(values[0]);
+    $item.find('.input-sesa input').val(values[2]);
+    $item.find('.input-reason').val(0);
+    $item.find("*[data-toggle='tooltip']").tooltip();
+  }
+  else{
+    var $sub = addSubElement($prototypeHolder);
+    $sub.find("*[data-toggle='tooltip']").tooltip();
+  }
+   //set default values
+   $sub.find('.input-activity select').val(values[5]);
+}
+
+function removeElement($element){
+  target = $element.attr('data-target');
+  $('*[data-content="'+target+'"]').remove();
+}
+
+function definePrototype($collectionHolder, level){
+  if (!$collectionHolder.attr('data-counter')) {
+    $collectionHolder.attr('data-counter', $collectionHolder.children().length);
+  }
+
+  var $prototype = $collectionHolder.attr('data-prototype');
+  var type = (level) ? "entry_name" : "activity_name";
+  var re = new RegExp(type,"g")
+  var $form = $prototype.replace(re, $collectionHolder.attr('data-counter')).replace(/time_name/g, parseInt($collectionHolder.attr('data-counter'))+1);
+
+  return $form;
+
+}
+
+function addSubElement($prototypeHolder){
+  var $element = definePrototype($prototypeHolder, false);
+  $prototypeHolder.append($element);
+  var $sub = $prototypeHolder.children().last();
+  var content = $prototypeHolder.attr('id')+'_'+$prototypeHolder.attr('data-counter');
+  attachData($sub, content);
+  var parentContent = $prototypeHolder.closest('tr').attr('data-content');
+  var $transfer = $sub.find('.transfer').attr({
+    'data-sub-target': $prototypeHolder.attr('id'),
+    'data-target': parentContent,
+    'data-disabled': 0});
+  $sub.find('.input-regular-hours input').val(8);
+  $sub.find('.input-overtime input').val(0);
+  //$sub.find('.input-zone select').val(0);
+  $prototypeHolder.attr('data-counter', Number($prototypeHolder.attr('data-counter')) + 1);
+
+  return $sub;
+}
+
+function attachData($item, content){
+  if (!$item.attr('data-content')) {
+      $item.attr('data-content', content);
+    }
+  $item.find('#rmv:last').attr('data-target', content);
+}
+
+function dynamicList(valueSelect){
+//delete all current field
+$('#entries').children().remove();
+
+//reset data counter
+$('#entries-prototype').attr('data-counter', 0);
+
+//set new overtime default
+switch(valueSelect){
+  case '2':
+    $('.input-ot-time input:first').val("13:00");
+    $('.input-ot-time input:last').val("01:00");
+    break;
+  case '3':
+    $('.input-ot-time input:first').val("19:30");
+    $('.input-ot-time input:last').val("07:30");
+    break;
+  default:
+    $('.input-ot-time input:first').val("07:30");
+    $('.input-ot-time input:last').val("19:30");
+}
+
+//add good nb fields
+for(var i = 0; i < employeesData.length; ++i){
+      if(employeesData[i][3] == $('.input-team').val() && employeesData[i][4] == valueSelect){
+        addElement($('#entries-prototype'), employeesData[i]);
+      }
+  }
+}
+
+function initCollection(){
+  if($.trim( $('#errors').html() ).length ) {
+    $('#errors').parent().toggleClass('hide');
+    alert('There are errors in this input, please check.');
+    }
+  else{
+    $('.input-ot-time input:first').val('07:30');
+    $('.input-ot-time input:last').val('19:00');
+    $('.input-shift').val(1);
+    $(".input-shift input[value=1]").attr('checked', 'checked');
+    for(var i = 0; i < employeesData.length; ++i){
+      if(employeesData[i][3] == $('.input-team').val() && employeesData[i][4] == $('.input-shift').val()){
+        addElement($('#entries-prototype'), employeesData[i]);
+      }
+    } 
+    $('form').children('div').last().hide();
+  }
+  $('.clockpicker').clockpicker(); 
+  $("*[data-toggle='tooltip']").tooltip();
+}
+
+function displayOvertime(start, end, $overt){
+  end = end.split(/:/);
+  start = start.split(/:/);
+  var diff = Math.round((end[0] * 3600 + end[1] * 60 + 43200 - (start[0] * 3600 + start[1] * 60 + 43200))/36)/100;
+  if (diff<0){
+    diff+=24;
+  }
+  $overt.val(diff);
+}
 
 jQuery.fn.preventDoubleSubmission = function() {
   $(this).on('submit',function(e){
@@ -245,3 +229,40 @@ jQuery.fn.preventDoubleSubmission = function() {
   // Keep chainability
   return this;
 };
+
+function presenceToggler($this){
+  var $main = $this.closest('#presence');
+  var cb1 =  $main.find('.input-present');
+  var cb2 =  $main.find('.input-halfday');
+  var $actProto = $main.closest('td').siblings('#activities').find('div:first');
+
+  if($this.attr('data-state') == 'Present'){
+    $this.find('.presence-gauge').animate({top: "+=15"}, 500);
+    cb1.val(cb1.prop('checked', false));
+    $this.attr('data-state','Absent').attr('data-original-title','Absent');
+
+    $this.closest('td').toggleClass('expand-cell');
+    $main.find('.toggling').toggleClass('hide').find('.input-reason').val(1);
+    $actProto.children().remove();
+
+  }else if($this.attr('data-state') == 'Absent'){
+    $this.find('.presence-gauge').animate({top: "-=8"}, 500);
+    cb1.val(cb1.prop('checked', true));
+    cb2.val(cb2.prop('checked', true));
+    $this.attr('data-state','Halfday').attr('data-original-title','Halfday');
+
+    addSubElement($actProto);
+    $actProto.children().last().find('.input-regular-hours input').val(4);
+     
+  }else{
+    $this.find('.presence-gauge').animate({top: "-=7"}, 500);
+    cb2.val(cb2.prop('checked', false));
+    $this.attr('data-state', 'Present').attr('data-original-title','Present');
+
+    $this.closest('td').toggleClass('expand-cell');
+    $main.find('.toggling').toggleClass('hide');
+    $main.find('.input-reason').val(0);
+    $actProto.children().last().find('.input-regular-hours input').val(8);
+  
+  }
+}
